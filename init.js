cbfb841ca17f45e37070939a11c7d0cd11f8a57f
Meli <135082123+MeliCynx@users.noreply.github.com>
2025-02-21 07:33:59 +0000

    basic shit started the website

---
+++ init.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    function fun(dir) {
+        return new Promise((resolve) => {
+            let p = document.createElement("script");
+            p.setAttribute("async", "");
+            p.src = dir;
+            p.onload = () => resolve(p);
+            p.onerror = function() {
+                location.reload;
+            }
+            document.head.appendChild(p);
+            console.log(dir);
+        });
+    }
+    async function init() {
+        console.log("all scripts loaded!")
+    }
+    init();
+
+}
